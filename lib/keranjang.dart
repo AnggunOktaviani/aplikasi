1a6bc45ee8abf820ef3bb195d959d7a4767ad473
anggunoktaviani <oanggun743gmail.com>
2023-05-17 14:48:10 +0700

    utspcsanggun

---
+++ lib/keranjang.dart
@@ -0,0 +1,101 @@
+import 'package:flutter/material.dart';
+
+class Keranjang extends StatelessWidget {
+  const Keranjang({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Keranjang'),
+      ),
+      body: Container(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            
+            Container(
+              child: Row(
+               crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Container(
+                  width: 680.0,
+                  decoration: BoxDecoration(color: Colors.green),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Image.asset('../images/celana1.webp',height: 150.0,width: 80,),
+                      Column(
+                        children: [
+                          Text('Celana'),
+                          Text('Rp. 190.000',style: TextStyle(color: Colors.red),)
+                        ],
+                      ),
+                      TextButton(onPressed: (){}, child: Text('Pesan',style: TextStyle(color: Colors.black,fontWeight: FontWeight.bold),)),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            ),
+            Container(
+              child: Row(
+               crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Container(
+                  width: 680.0,
+                  decoration: BoxDecoration(color: Colors.green),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Image.asset('../images/jaket1.webp',height: 150.0,width: 80,),
+                      Column(
+                        children: [
+                          Text('Jaket Hitam'),
+                          Text('Rp. 120.000',style: TextStyle(color: Colors.red),)
+                        ],
+                      ),
+                      TextButton(onPressed: (){}, child: Text('Pesan',style: TextStyle(color: Colors.black,fontWeight: FontWeight.bold),)),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            ),
+            Container(
+              child: Row(
+               crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Container(
+                  width: 680.0,
+                  decoration: BoxDecoration(color: Colors.green),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Image.asset('../images/kaos2.webp',height: 150.0,width: 80,),
+                      Column(
+                        children: [
+                          Text('Kaos'),
+                          Text('Rp. 80.000',style: TextStyle(color: Colors.red),)
+                        ],
+                      ),
+                      TextButton(onPressed: (){}, child: Text('Pesan',style: TextStyle(color: Colors.black,fontWeight: FontWeight.bold),)),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            ),
+            ElevatedButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    child: Text('Kembali'),
+                  ),
+                  SizedBox(height: 10.0,)
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/tshirt.dart
@@ -0,0 +1,169 @@
+import 'package:aplikasi_sederhana/keranjang.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class Tshirt extends StatelessWidget {
+  const Tshirt({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('T-Shirt'),
+      ),
+      body: ListView(
+        children: [
+          Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Image.asset(
+                '../images/kaos.webp',
+                height: 400.0,
+              ),
+              SizedBox(height: 10.0),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Column(
+                    children: [
+                      Column(
+                        children: [
+                          Image.asset(
+                            '../images/kaos1.webp',
+                            width: 250,
+                            height: 200.0,
+                          ),
+                          SizedBox(
+                            height: 10.0,
+                          ),
+                          Text(
+                            'Rp. 90.000',
+                            style: TextStyle(
+                              color: Colors.red,
+                            ),
+                          ),
+                          TextButton(
+                              onPressed: () {
+                                Navigator.push(context,
+                              MaterialPageRoute(builder: (context) {
+                            return Keranjang();
+                          }));
+                              }, child: Text('PESAN SEKARANG'))
+                        ],
+                      ),
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      Column(
+                        children: [
+                          Image.asset(
+                            '../images/kaos1.webp',
+                            width: 250,
+                            height: 200.0,
+                          ),
+                          SizedBox(
+                            height: 10.0,
+                          ),
+                          Text(
+                            'Rp. 90.000',
+                            style: TextStyle(
+                              color: Colors.red,
+                            ),
+                          ),
+                          TextButton(
+                              onPressed: () {
+                                Navigator.push(context,
+                              MaterialPageRoute(builder: (context) {
+                            return Keranjang();
+                          }));
+                              }, child: Text('PESAN SEKARANG'))
+                        ],
+                      ),
+                    ],
+                  )
+                ],
+              ),
+              SizedBox(
+                height: 15.0,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Column(
+                    children: [
+                      Column(
+                        children: [
+                          Image.asset(
+                            '../images/kaos1.webp',
+                            width: 250,
+                            height: 200.0,
+                          ),
+                          SizedBox(
+                            height: 10.0,
+                          ),
+                          Text(
+                            'Rp. 90.000',
+                            style: TextStyle(
+                              color: Colors.red,
+                            ),
+                          ),
+                          TextButton(
+                              onPressed: () {Navigator.push(context,
+                              MaterialPageRoute(builder: (context) {
+                            return Keranjang();
+                          }));
+                          }, child: Text('PESAN SEKARANG'))
+                        ],
+                      ),
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      Column(
+                        children: [
+                          Image.asset(
+                            '../images/kaos1.webp',
+                            width: 250,
+                            height: 200.0,
+                          ),
+                          SizedBox(
+                            height: 10.0,
+                          ),
+                          Text(
+                            'Rp. 90.000',
+                            style: TextStyle(
+                              color: Colors.red,
+                            ),
+                          ),
+                          TextButton(
+                              onPressed: () {
+                                Navigator.push(context,
+                              MaterialPageRoute(builder: (context) {
+                            return Keranjang();
+                          }));
+                              }, child: Text('PESAN SEKARANG'))
+                        ],
+                      ),
+                    ],
+                  )
+                ],
+              ),
+              SizedBox(
+                height: 10.0,
+              ),
+               ElevatedButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    child: Text('Kembali'),
+                  ),
+                  SizedBox(height: 10.0,)
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
